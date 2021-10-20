e976949818e0db9a241b5e5d132f9598f7e8b94f
Joshua Garcia <garciajoshua907@yahoo.com>
2021-10-20 12:05:44 -0600

    changes colors

--- src/components/SkillsDisplay.js
+++ src/components/SkillsDisplay.js
@@ -3,33 +3,20 @@ import React from "react";
 export default function SkillsDisplay() {
   return (
     <div className="skills-container">
-      <h2 className="heading"> -Skills-</h2>
+      <h2 className="heading">What Iv'e Worked With</h2>
       <div className="skills-display">
         <ol className="list-1">
-          <li>
-            <b>languages:</b>
-          </li>
           <li>-HTML</li>
           <li>-CSS</li>
           <li>-JavaScript</li>
-          <li>-REACT 'library'</li>
-        </ol>
-        <ol className="list-3">
-          <li>
-            <b>Worked With:</b>
-          </li>
+          <li>-REACT</li>
+          <li>-Github/pages</li>
           <li>-Git</li>
           <li>-Github</li>
           <li>-Linux</li>
           <li>-Boot-strap</li>
           <li>-JavaScript libraries</li>
-          <li>-API</li>
-        </ol>
-        <ol className="list-2">
-          <li>
-            <b>Deployed:</b>
-          </li>
-          <li>-Github/pages</li>
+          <li>-API's</li>
           <li>-Netlify</li>
           <li>-Heroku</li>
         </ol>
